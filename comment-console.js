726f92015f4bdfe786e2c3a943190de1af1fe224
imran-binhasan <imranbinhasan.work@gmail.com>
2025-09-16 12:54:33 +0600

    add console & comment types

---
+++ comment-console.js
@@ -0,0 +1,9 @@
+
+/* multi line
+ comment */ 
+
+ // single line comment
+
+ console.log("This is a console log message.");
+ console.error("This is an error message.");
+ console.warn("This is a warning message.");
