15fc262328f1fbd6fb6e29561d7f02f7baadf14b
imran-binhasan <imranbinhasan.work@gmail.com>
2025-09-16 22:11:40 +0600

    add js variable types

---
+++ variable.js
@@ -0,0 +1,21 @@
+// function scope (var)
+var greeting = "hello"
+greeting = "hi"
+
+
+// block scope (let & const)
+
+let number = 5
+number = 7
+
+const condition = true
+// condition = false
+
+if(true) {
+    console.log(x)
+     var x = 10
+     console.log(x)
+     var x = 11
+    
+}
+console.log(x)
