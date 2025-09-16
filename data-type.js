5039b97932061e6c09ce4fc94275217fbe28ef60
imran-binhasan <imranbinhasan.work@gmail.com>
2025-09-16 13:26:00 +0600

    add data-types

---
+++ data-type.js
@@ -0,0 +1,27 @@
+// primitive data types : 7
+// immutable, stored by value
+
+string
+boolean
+number
+undefined
+null
+bigint
+symbol
+
+// non-primitive data types : infinite
+// mutable, stored by reference
+Object
+Array
+Function
+Date
+RegExp
+Error
+Set
+Map
+WeakMap
+WeakSet
+Promise
+
+// everything that doesn't fall under primitive is basically non primitive
+
